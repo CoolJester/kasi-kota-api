b9e260945e08dfd527b716555662371c1d02c35c
Ntsako Baloyi <ntsako1616@gmail.com>
2025-07-31 20:58:01 +0200

    Update url paths and remove endpoint versioning for now

--- routes/order.js
+++ routes/order.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController');
 
-router.get('/v1/order/:orderId', orderController.getOrderByOrderId);
-router.get('/v1/order/user/:userId', orderController.getOrderByUserId);
-router.post('/v1/order', orderController.placeOrder);
-router.delete('/v1/order/:orderId', orderController.deleteOrder);
+router.get('/order/:orderId', orderController.getOrderByOrderId);
+router.get('/order/user/:userId', orderController.getOrderByUserId);
+router.post('/order', orderController.placeOrder);
+router.delete('/order/:orderId', orderController.deleteOrder);
 
 
 module.exports = router;
